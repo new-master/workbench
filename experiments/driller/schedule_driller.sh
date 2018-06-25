5ff584f5ba58e31b477ceba946be910ffa45678d
new-master <ccheng55@asu.edu>
2018-06-25 23:05:45 +0000

    adaption to list of binaries

--- experiments/driller/schedule_driller.sh
+++ experiments/driller/schedule_driller.sh
@@ -7,10 +7,10 @@ export CPU_REQUEST=5000m
 export CPU_LIMIT=6000m
 export TIMEOUT=28800
 
-curl https://raw.githubusercontent.com/zardus/cgc-bins/master/lists/all.txt | parallel -j100 -k '
+curl https://raw.githubusercontent.com/new-master/cgc-bin/master/lists/all_adv.txt | parallel -j100 -k '
 NAME=$($WORKBENCH_DIR/scripts/kubesanitize '$TAG'-{});
 $WORKBENCH_DIR/scripts/make_pod -l $NAME " \
-	wget https://github.com/zardus/cgc-bins/raw/master/all_unpatched/{} -O /home/angr/{}; \
+	wget https://github.com/new-master/cgc-bin/tree/master/adv_patch/{} -O /home/angr/{}; \
 	chmod 755 /home/angr/{}; \
 	/home/angr/.virtualenvs/angr/bin/shellphuzz /home/angr/{} \
 		-t '$TIMEOUT' -C \
